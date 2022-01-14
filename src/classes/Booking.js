beb642d9b009259fbe936448d08e0da7ffacf05f
Jacob Quintana <jacobquintaa@gmail.com>
2022-01-14 15:43:35 -0700

    Create booking class

--- src/classes/Booking.js
+++ src/classes/Booking.js
@@ -1,22 +1,25 @@
-class Bookings {
+class Booking {
   constructor(bookingData, roomData) {
     this.bookingData = bookingData;
     this.roomData = roomData;
   }
-  getBookingRoom(roomNumber) {
-
-    
-    // should take in a booking room number 
-    // compare the room number from the booking data and room data 
-    //match the booking to the right room 
-    // return the single room object
+  getUserBookings(userId) {
+    let findBookings = this.bookingData.filter((booking) => {
+        return booking.userID === userId
+    })
+    return findBookings
   }
 
-  getBookingDate() {
-    //takes in an argument of a booking id
-    //return the booking date that matches the id
-    // 
+  getBookingRoom(bookingId) {
+    let findBooking = this.bookingData.find((booking) => {
+      return booking.id === bookingId
+    })
+
+    let findRoom = this.roomData.find((room) => {
+        return room.number ===  findBooking.roomNumber
+    })
+    return findRoom
   }
 }
 
-module.exports = Bookings ;
+module.exports = Booking ;
