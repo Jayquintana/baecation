036f1c79987a27734f1c98c9448fa018f22e5c07
Jacob Quintana <jacobquintaa@gmail.com>
2022-01-13 22:05:37 -0700

    Create classes and test"

--- src/classes/Booking.js
+++ src/classes/Booking.js
@@ -1,6 +1,21 @@
 class Bookings {
-  constructor() {
+  constructor(bookingData, roomData) {
+    this.bookingData = bookingData;
+    this.roomData = roomData;
+  }
+  getBookingRoom(roomNumber) {
 
+    
+    // should take in a booking room number 
+    // compare the room number from the booking data and room data 
+    //match the booking to the right room 
+    // return the single room object
+  }
+
+  getBookingDate() {
+    //takes in an argument of a booking id
+    //return the booking date that matches the id
+    // 
   }
 }
 
